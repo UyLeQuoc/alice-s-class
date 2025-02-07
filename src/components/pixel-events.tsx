c52117942adb9fbf02dcebf987c8e9d341f6fc8e
Le Quoc Uy <lequocuyit@gmail.com>
2025-02-07 23:51:51 +0700

    Refactor Facebook Pixel integration for improved initialization and tracking

--- src/components/pixel-events.tsx
+++ src/components/pixel-events.tsx
@@ -1,18 +1,19 @@
 "use client";
 import React, { useEffect } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
+import ReactPixel from "react-facebook-pixel";
 
 export const FacebookPixelEvents: React.FC = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init("996511195660551"); //don't forget to change this
-        ReactPixel.pageView();
-      });
+    ReactPixel.init("996511195660551"); 
+    ReactPixel.pageView();
+  }, []);
+
+  useEffect(() => {
+    ReactPixel.pageView();
   }, [pathname, searchParams]);
 
   return null;
